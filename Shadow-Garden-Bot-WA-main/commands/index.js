fe94f6bf3fccd50ae1aa874d65e34eccc8b4f230
ryukcodes333 <ibraheemyakub38@gmail.com>
2026-07-31 05:57:28 +0100

    Update OWNER_LID for bot configuration

--- Shadow-Garden-Bot-WA-main/commands/index.js
+++ Shadow-Garden-Bot-WA-main/commands/index.js
@@ -36,7 +36,7 @@ const { buildLinkPreview } = require('../linkPreviewHelper')
 
 const PREFIX      = global.prefix   || '.'
 const POKE_PREFIX = '#'
-const OWNER_LID   = '75454690107579@lid'
+const OWNER_LID   = '6125344813179@lid'
 
 const spamTracker = {}
 
